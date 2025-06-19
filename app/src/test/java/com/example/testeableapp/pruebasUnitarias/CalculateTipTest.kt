bd4c76251ee7fe46dec4e234a5819d34cf476ce2
Gaba3 <00103923@uca.edu.sv>
2025-06-19 16:15:39 -0600

    Refactoring packages

--- app/src/test/java/com/example/testeableapp/pruebasUnitarias/CalculateTipTest.kt
+++ app/src/test/java/com/example/testeableapp/pruebasUnitarias/CalculateTipTest.kt
@@ -1,7 +1,7 @@
-package com.example.testeableapp.Pruebas_Unitarias
+package com.example.testeableapp.pruebasUnitarias
 
 import com.example.testeableapp.ui.Screens.calculateTip
-import org.junit.Assert.assertEquals
+import org.junit.Assert
 import org.junit.Test
 
 class CalculateTipTest {
@@ -10,21 +10,21 @@ class CalculateTipTest {
     @Test
     fun test_calculateTip_20Percent(){
         val result = calculateTip(amount = 100.0, tipPercent = 20, roundUp = false)
-        assertEquals(20.0, result, 0.01)
+        Assert.assertEquals(20.0, result, 0.01)
     }
 
     //#2 Calculo del 15% y redondeo
     @Test
     fun test_calculateTip_15PercentWithRounding(){
         val result = calculateTip(amount = 100.0, tipPercent = 15, roundUp = true)
-        assertEquals(15.0, result, 0.01)
+        Assert.assertEquals(15.0, result, 0.01)
     }
 
     //#3 Calculo con cantidad negativa
     @Test
     fun test_calculateTip_negativeAmount(){
         val result = calculateTip(amount = -50.0, tipPercent = 20, roundUp = false)
-        assertEquals(0.0, result, 0.01)
+        Assert.assertEquals(0.0, result, 0.01)
     }
 
     //#4 Total a pagar por persona
@@ -34,6 +34,6 @@ class CalculateTipTest {
         val tip = calculateTip(amount, 20, false)
         val numberOfPeople = 4
         val totalPerPerson = (amount + tip)/ numberOfPeople
-        assertEquals(30.0, totalPerPerson, 0.01)
+        Assert.assertEquals(30.0, totalPerPerson, 0.01)
     }
 }
